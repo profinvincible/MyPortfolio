ab29656d61df8c05c02d1ba68a0efbb5e0daee61
ProfInvincible <muoghaluijeoma9@gmail.com>
2025-01-10 19:24:54 +0100

    lastScrollButtonSection

--- src/components/buttons/allButtons.js
+++ src/components/buttons/allButtons.js
@@ -4,7 +4,7 @@ export const MoreAboutMeButton = () => {
   return (
     <a
       href
-      className="inline-flex justify-center items-center button text-black font-bold text-lg no-underline mt-5 bg-[#0eff] hover:bg-[#0eff] hover:shadow-[0_0_30px_#0eff] transition-all duration-300 rounded-lg">
+      className="inline-flex justify-center items-center button text-black font-bold text-lg no-underline mt-1 bg-[#0eff] hover:bg-[#0eff] hover:shadow-[0_0_30px_#0eff] transition-all duration-300 rounded-lg">
       <button className="py-2 px-4">More About Me</button>
     </a>
   );
