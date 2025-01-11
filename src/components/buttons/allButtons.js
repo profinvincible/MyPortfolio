424b7d340cf655dfe4ea53b1c0c6f58cdefe9acf
ProfInvincible <muoghaluijeoma9@gmail.com>
2025-01-11 10:46:49 +0100

    finalHurdle

--- src/components/buttons/allButtons.js
+++ src/components/buttons/allButtons.js
@@ -3,7 +3,7 @@ import React from "react";
 export const MoreAboutMeButton = () => {
   return (
     <a
-      href="www.example.com"
+      href="#about"
       className="inline-flex justify-center items-center button text-black font-bold text-lg no-underline mt-1 bg-[#0eff] hover:bg-[#0eff] hover:shadow-[0_0_30px_#0eff] transition-all duration-300 rounded-lg">
       <button className="py-2 px-4">More About Me</button>
     </a>
@@ -13,7 +13,7 @@ export const MoreAboutMeButton = () => {
 export const LearnMore = () => {
   return (
     <a
-      href="www.example.com"
+      href=""
       className="inline-flex justify-center items-center button text-black font-bold text-lg no-underline mt-5 bg-[#0eff] hover:bg-[#0eff] hover:shadow-[0_0_30px_#0eff] transition-all duration-300 rounded-lg">
       <button className="py-2 px-4">Learn More</button>
     </a>
